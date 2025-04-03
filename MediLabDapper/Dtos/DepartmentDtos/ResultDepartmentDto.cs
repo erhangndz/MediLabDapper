3fb80e01f9049c82990ce1f36eea7cc59485508c
Erhan Gündüz <erhangndz@live.com>
2025-04-03 22:39:32 +0300

    Add project files.

---
+++ MediLabDapper/Dtos/DepartmentDtos/ResultDepartmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MediLabDapper.Dtos.DepartmentDtos
+{
+    public class ResultDepartmentDto
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ MediLabDapper/Extensions/ServiceRegistrations.cs
@@ -0,0 +1,22 @@
+﻿using MediLabDapper.Context;
+using MediLabDapper.Repositories.DepartmentRepositories;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace MediLabDapper.Extensions
+{
+    public static class ServiceRegistrations
+    {
+
+
+
+        public static IServiceCollection AddRepositoriesExt(this IServiceCollection services)
+        {
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<DapperContext>();
+
+            return services;
+        }
+
+
+    }
+}
